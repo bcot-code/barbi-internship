cbc11b9e53cce8c95c61ac927bee64cd9ca0aec8
bcot-code <78989054+bcot-code@users.noreply.github.com>
2026-01-26 21:17:03 -0600

    init ItemDetails, fix bug in AuthorItem.jsx

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,83 +1,137 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
+import React,{useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "../UI/Skeleton";
+import "keen-slider/keen-slider.min.css"
+import { useKeenSlider } from "keen-slider/react"
 
 const AuthorItems = ({authorId}) => {
-
-  const [authors, setAuthors] = useState(null);
+  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() =>{
-    const fetchAuthors = async () => {
+  // Slider configuration
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      loop: true,
+      slides: {
+        perView: 4,
+        spacing: 15,
+      },
+      rubberband: true,
+      breakpoints: {
+        "(max-width: 991px)": {
+          slides: { perView: 2, spacing: 15 },
+        },
+        "(max-width: 575px)": {
+          slides: { perView: 1, spacing: 15 },
+        },
+      },
+    },
+    []
+  )
+
+  useEffect(() => {
+    const fetchAuthorItems = async () => {
       try{
-        const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems`);
-        setAuthors(data);
-      }
-      catch(err) {
-        console.error("Failed to fetch NFT");
-      } finally {
-        setLoading(false);
+        console.log("Fetching items for author ID:", authorId);
+        const res = await fetch(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`);
+        const data = await res.json();
+        console.log("Fetched author items:", data);
+        console.log("NFT Collection:", data.nftCollection);
+        setItems(data.nftCollection || []);
+      } catch(err){
+        console.error("Error fetching author items:", err);
+      } finally{
+        setTimeout(() => {
+          setLoading(false);
+        }, 1100);
       }
     };
-    fetchAuthors();
-  },[authorId]);
-  console.log("Author Id:", authorId)
+    if (authorId) {
+      fetchAuthorItems();
+    }
+  }, [authorId]);
 
-  if (loading) return <p>Loading...</p>;
-  if (!authors) return <p>Item not found</p>;
+
+  if (loading) {
+    return (
+      <div className="keen-slider">
+        {[...Array(4)].map((_, i) => (
+          <div className="keen-slider__slide" key={i}>
+            <Skeleton width="100%" height="250px" />
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <div className="de_tab_content">
       <div className="tab-1">
-        <div className="row">
-          {authors.map((author, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={author.authorImage} alt={author.title} />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+        <div className="slider-container">
+          <div ref={sliderRef} className="keen-slider">
+            {items.map((item) => (
+              <div className="keen-slider__slide" key={item.id}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link to="">
+                      <img 
+                        className="img-fluid"
+                        loading="lazy"
+                        src={item.nftImage} 
+                        alt={item.title} 
+                      />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <img
+                        src={item.nftImage}
+                        className="lazy nft__item_preview"
+                        alt={item.title}
+                        loading="lazy"
+                      />
+                    </Link>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={author.nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>97</span>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.nftId}`}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="slider-arrows">
+            <button onClick={() => instanceRef.current?.prev()}>
+              ‹
+            </button>
+            <button onClick={() => instanceRef.current?.next()}>
+              ›
+            </button>
+          </div>
         </div>
       </div>
     </div>
